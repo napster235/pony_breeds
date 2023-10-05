184feb76880c33474413b106bf111d7d1b51a044
Dragomir, Tudor <dragomir.tuddor@gmail.com>
2023-10-05 16:05:59 +0300

    Remove webmock config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require 'breeds'
-require 'webmock/rspec'
-
-WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
